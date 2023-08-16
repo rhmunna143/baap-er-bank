b658a46b483f1ada43990722f24c20fc7a0902f6
rhmunna143 <rhmunna19@gmail.com>
2023-08-16 16:17:19 +0600

    login panel added

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ document.getElementById('login-btn').addEventListener('click', function(){
 
     const userPassword = document.getElementById('input-password').value;
 
-    
+    let user1Email = 'choto-chele@hotmail.beshi';
+    let user1Password = 'psswd.debona';
+
+    if(userEmail == user1Email && userPassword == user1Password){
+       return window.location.assign('http://127.0.0.1:5500/home.html');
+    } else{
+        alert('Wrong Email or Password!!!')
+    }
 })
