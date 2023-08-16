64a68e56d7e8d9f29b9f9009e5ebb1e32e40f881
rhmunna143 <rhmunna19@gmail.com>
2023-08-16 13:08:51 +0600

    login half

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('login-btn').addEventListener('click', function(){
+    const userEmail = document.getElementById('input-email').value;
+
+    const userPassword = document.getElementById('input-password').value;
+
+    
+})
